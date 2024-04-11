f1e06a3adcc9ccb1a1efad93e2394eadbde8d60a
Informatica in Pillole <162987923+informaticaInPillole@users.noreply.github.com>
2024-04-11 16:14:08 +0200

    Update Remove-DuplicateFiles.ps1

--- Remove-DuplicateFiles.ps1
+++ Remove-DuplicateFiles.ps1
@@ -1,30 +1,64 @@
 [Cmdletbinding()]
 param(
-    [string[]] $Paths
+    # Array of paths to search for duplicates
+    [string[]] $Paths,
+    # Switch parameter to enable searching subfolders
+    [switch] $Recurse
 )
 
-$hashtable = [System.Collections.Generic.Dictionary[string,System.Collections.Generic.List[string]]]::new()
+# Initialize variables
+$files = New-Object System.Collections.Hashtable    # Hash table to store hash and file paths
+$bytes = 0                                          # Total size of duplicate files (bytes)
+$duplicates = 0                                     # Number of duplicate files found 
 
+# Loop through each provided path
 foreach ($path in $Paths) {
+    # Check if path exists
     if(!(Test-Path $path))
     {
         Write-Host "Path $path not found"
         continue;
     }
 
-    $items = Get-ChildItem -Path $path -Recurse
-    Write-Host "Found $($items.Count) items"
-    
-    foreach ($item in $items) {
+    # Get all files from the path (including subfolders if Recurse is set)
+    $items = Get-ChildItem -Path $path -Recurse:$Recurse -File
 
+    Write-Host "$($items.Count) items to analyze"
+
+    # Progress bar initialization
+    $i = 1
+    Write-Progress -Activity "Evaluating $($items.Count) files [path: $path]" -Status ([string]::Format("Duplicates: {0} ({1:N2} Mb)", $duplicates, $bytes/1Mb)) -PercentComplete 0
+
+    # Loop through each file in the path
+    foreach ($item in $items) {
+        Write-Progress -Activity "Evaluating $i of $($items.Count) files [path: $path]" -Status ([string]::Format("Duplicates: {0} ({1:N2} Mb)", $duplicates, $bytes/1Mb)) -PercentComplete ($i++/$items.Count * 100)
+
+        # Calculate SHA256 hash of the file
         $hash = (Get-FileHash -Path $item.FullName -Algorithm SHA256).Hash
-        if(!$hashtable.ContainsKey($hash))
-        {
-            $hashtable.Add($hash, [System.Collections.Generic.List[string]]::new())
+
+        # Check if hash exists in the hash table
+        if($files.ContainsKey($hash)){
+            # Found duplicate! Increment counters
+            $duplicates++;
+            $bytes += $item.Length
         }
 
-        $hashtable[$hash] += $item.FullName
+        # Add file path to the hash table entry for the current hash
+        $files[$hash] += @($item.FullName)
+       
     }
-    
 }
-$hashtable.Values | ?{$_.Count -gt 1}
+# Display summary of duplicate files found
+Write-Host ([string]::Format("Duplicates found: {0} ({1:N2} Mb)", $duplicates, $bytes/1Mb))
+
+# Filter keys from the hash table where there are multiple files (duplicates)
+$keys = $files.Keys | Where-Object { $files[$_].Count -gt 1 }
+# Create a new hash table to store duplicate file information
+$duplicateFiles = New-Object System.Collections.Hashtable
+foreach($key in $keys)
+{
+    # Add key (hash) and corresponding file paths to the duplicate files hash table
+    $duplicateFiles[$key] = $files[$key]
+}
+# Output the hash table containing duplicate file information
+Write-Output $duplicateFiles
